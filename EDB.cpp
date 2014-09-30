286247704fd54da27cb28e6fd8c6a1255fcb637d
thirsty fish <feky001@gmail.com>
2014-09-30 17:28:21 +1000

    EDB::open fix

--- EDB.cpp
+++ EDB.cpp
@@ -85,6 +85,7 @@ EDB_Status EDB::create(unsigned long head_ptr, unsigned long tablesize, unsigned
 EDB_Status EDB::open(unsigned long head_ptr)
 {
   EDB_head_ptr = head_ptr;
+  EDB_table_ptr = sizeof(EDB_Header) + EDB_head_ptr;
   readHead();
   return EDB_OK;
 }
